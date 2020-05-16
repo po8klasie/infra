8417451459cfad2aa1ea308be8a5ce9882677148
Michał Łazowik <mlazowik@gmail.com>
2020-05-16 18:03:48 +0200

    Use docker-compose container

--- update-backend.sh
+++ update-backend.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+alias docker-compose=docker run -it --rm \
+	-v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:$PWD" -w="$PWD" \
+	docker/compose:1.25.5
+
 sudo docker-compose pull backend
 sudo docker-compose up -d backend
