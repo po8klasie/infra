6cf04c512994ce8c426e009863ef2f887dc18fac
Michał Łazowik <mlazowik@gmail.com>
2020-05-16 18:18:05 +0200

    Fix too much quote escaping

--- update-frontend.sh
+++ update-frontend.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-COMPOSE_CMD=(docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:$PWD" "-w=\"$PWD\"" docker/compose:1.25.5)
+COMPOSE_CMD=(docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:$PWD" -w="$PWD" docker/compose:1.25.5)
 
 "${COMPOSE_CMD[@]}" pull frontend
 "${COMPOSE_CMD[@]}" up -d frontend
