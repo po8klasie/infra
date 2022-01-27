5b1973f8aa0f804c815735fd0d099698ba6dcc04
Michał Oręziak <micorix.dev@gmail.com>
2022-01-27 23:20:07 +0100

    Add proxy headers and remove Cache-Control header (#36)
    * Add proxy headers and remove Cache-Control header
    
    * also add --force-recreate to update-all command
    
    * Prefix frontend upstream with http://

--- app/update-all.sh
+++ app/update-all.sh
@@ -3,4 +3,4 @@
 COMPOSE_CMD=(docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v "$PWD:$PWD" -w="$PWD" docker/compose:1.25.5)
 
 "${COMPOSE_CMD[@]}" pull
-"${COMPOSE_CMD[@]}" up -d
+"${COMPOSE_CMD[@]}" up --force-recreate -d # force-recreate bc of nginx conf stored in the repo
